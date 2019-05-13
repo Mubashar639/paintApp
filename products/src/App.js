5759bc19b932cacc10f6b661d9a1ddc80da79f4b
Hassan Habib Tahir <hassanhabibtahir@gmail.com>
2019-05-13 16:56:05 +0500

    changing

--- products/src/App.js
+++ products/src/App.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
+
 import CardImages from './Component/card/card'
 function App() {
   return (
     <div>
     <CardImages/>
-=======
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> bf879a138c40d21c1139481dc989c1cab7c09c80
-    </div>
-  );
+</div>
+  )
 }
 
 export default App;
